6385d298e56f2d421bec84e8f68d218b3e2b51c8
selvakumar k <84513538+selvakumar-MERN@users.noreply.github.com>
2023-07-14 09:58:31 +0530

    Add files via upload

---
+++ src/Components/Invalid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Invalid(props) {
+    return (
+        <div className='container'>
+            <h2>Sorry Invalid Link or invalid token</h2>
+            
+            
+        </div>
+    );
+}
+
+export default Invalid;
